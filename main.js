6581f6ac16ccff0d00109e582bdf1ddca49651a4
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-18 11:56:15 +0530

    impl

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-const path = require("path");
 const fs = require("fs");
 
-// TODO: Use path.join to join two file path segments
-const outputPath = path.join(__dirname, "output.txt");
+// TODO: Write the message "Hello, World!" to the file "output.txt"
 
-const message = "Hello, World!";
-
-// Write the message to the file
-fs.writeFile(outputPath, message, (err) => {
-  if (err) {
-    console.error(`Error writing to file: ${err}`);
-    throw err;
+fs.writeFile("./output.txt", "Hello, World!", (error) => {
+  if (error) {
+    console.log(error);
   }
-  console.log(`Message "${message}" has been written to ${outputPath}`);
 });
-// TODO: Print the resulting path to the console
